598a6271b49a091354b5ee71b7c1e87001fa9b08
sethcarrino <sethcarrino@gmail.com>
2017-07-18 15:50:17 -0400

    modified index.html and main.js

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
+let recipeBox = document.querySelector(".recipeContainer")
+let recipes = document.querySelector(".recipeList")
 let content = document.querySelector("#wrapper");
-let recipeHTML = "";
+let button = document.querySelector("#button");
 
-fetch("http://www.recipepuppy.com/api/?q=pizza")
 
-.then(function(response){
-  if (response === 200){
-    return response.json();
-  }
-})
+fetch(`https://crossorigin.me/http://www.recipepuppy.com/api/?q=${search.value}`)
 
-.then(function(data) {
-  let data = data.results;
-  for (var i = 0; i < data.length; i++) {
-    recipeHTML += `
-    <div id="container">
-      <img src="${data.thumbnail}">
-    </div>`
-  }
-})
+  .then(function(response) {
+    if (response.status === 200) {
+      return response.json();
+    }
+  })
+
+  .then(function(data) {
+    console.log(data);
+    let recipeHTML = "";
+    let results = data.results;
+    for (var i = 0; i < results.length; i++) {
+      recipes.innerHTML += `
+    <img src=${results[i].thumbnail}>
+    <h1>${results[i].title}</h1>
+    `
+    }
+
+  })
+
+
+
+
+  .catch(function(error) {
+    console.error(error.message);
+  });
